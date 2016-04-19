47d80ad4bf08713f322447d0e837a35472e2c99d
Corey McDaniel <corey.r.mcdaniel@live.com>
2016-04-18 17:50:39 -0700

    Worked through Routing guide.

---
+++ web/router.ex
@@ -0,0 +1,37 @@
+defmodule HelloPhoenix.Router do
+  use HelloPhoenix.Web, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", HelloPhoenix do
+    pipe_through :browser # Use the default browser stack
+
+    get "/", PageController, :index
+    resources "/reviews", ReviewController
+  #  resources "/posts", PostController, only: [:index, :show]
+  #  resources "/comments", CommentController, except: [:delete]
+  #  get "/hello", HelloController, :index
+  #  get "/hello/:messenger", HelloController, :show
+  end
+  
+  scope "/admin", HelloPhoenix.Admin, as: :admin do
+    pipe_through :browser
+    
+    resources "/reviews", ReviewController
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", HelloPhoenix do
+  #   pipe_through :api
+  # end
+end
