232163e24089ccfdc6db09355d8cbc37d0d7fb10
Corey McDaniel <corey.r.mcdaniel@live.com>
2016-04-19 23:30:54 -0700

    Working on Views guide

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,7 +1,33 @@
 defmodule HelloPhoenix.PageController do
   use HelloPhoenix.Web, :controller
 
-  def index(conn, _params) do
-    render conn, "index.html"
+# plug :action
+# plug :render when action in [:index]
+
+  def index(conn, params) do
+    conn
+    #|> put_flash(:info, "Welcome to Phoenix, from flash info!")
+    #|> put_flash(:error, "Let's pretend to have an error.")
+    #|> put_layout("admin.html")
+    |> render "index.html"
+    #render conn, "index.text", message: params["message"]
+    #redirect conn, to: "/redirect_test"
+  end
+  
+  def redirect_test(conn, _params) do
+    text conn, "Redirect!"
+  end
+  
+  def show(conn, %{"id" => id}) do
+      html conn, """
+     <html>
+       <head>
+          <title>Passing an Id</title>
+       </head>
+       <body>
+         <p>You sent in id #{id}</p>
+       </body>
+     </html>
+    """
   end
 end
--- web/router.ex
+++ web/router.ex
@@ -2,7 +2,7 @@ defmodule HelloPhoenix.Router do
   use HelloPhoenix.Web, :router
 
   pipeline :browser do
-    plug :accepts, ["html"]
+    plug :accepts, ["html", "text"]
     plug :fetch_session
     plug :fetch_flash
     plug :protect_from_forgery
@@ -17,7 +17,9 @@ defmodule HelloPhoenix.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
-    resources "/reviews", ReviewController
+    get "/redirect_test", PageController, :redirect_test, as: :redirect_test
+  #  get "/show-id/:id", PageController, :show
+  #  resources "/reviews", ReviewController
   #  resources "/posts", PostController, only: [:index, :show]
   #  resources "/comments", CommentController, except: [:delete]
   #  get "/hello", HelloController, :index
